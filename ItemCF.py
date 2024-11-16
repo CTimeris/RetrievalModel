d0112072763a4993735530ac93b3e348f00565c7
tongc <944130769@qq.com>
2024-11-16 17:25:46 +0800

    实现

--- ItemCF.py
+++ ItemCF.py
@@ -1,69 +1,74 @@
 import math
+import pickle
 from collections import defaultdict
 from tqdm import tqdm
 
 
 def ItemCF_sim(user_item_dict):
     """
-        物品与物品之间的相似性矩阵计算
-        输入: user_item_dict: 用户交互序列  {user1:{item1, item2, ...}, user2...}
-        返回: 相似性矩阵
+    物品与物品之间的相似性矩阵计算
+    :param user_item_dict: 用户交互序列  {user1:{item1, item2, ...}, user2...}
+    :return: 相似性矩阵
 
-        base公式：已有i，计算与j的相似度 = 同时喜欢用户i和j的用户数 / 喜欢i的用户数
+    base公式：已有i，计算与j的相似度 = 同时喜欢用户i和j的用户数 / 喜欢i的用户数
         问题：若j热门物品，则与任何物品相似度都高
         打压热门物品：除以一个j被喜欢的数量：同时喜欢用户i和j的用户数 / 根号（喜欢i的用户数 * 喜欢j的用户数）
-        进一步打压活跃用户：上面的分子 相当于每个用户权重都为1，可以根据其交互数n设置权重（1/log(1+n)）
+        进一步打压活跃用户：上面的分子 相当于每个用户权重都为1，可以根据其交互数n设置权重 (1/log(1+n))
     """
-    # 计算物品相似度
     i2i_sim = {}
-    item_cnt = defaultdict(int)     # 物品的总点击次数（喜欢物品i的用户总数）
+    item_cnt = defaultdict(int)  # 物品的总点击次数（喜欢物品i的用户总数）
 
     for user, item_list in tqdm(user_item_dict.items()):
         for i in item_list:
             item_cnt[i] += 1
-            i2i_sim.setdefault(i, {})   # setdefault查找指定键,存在则返回对应的值,不存在则在字典中添加该键，值设为默认值
+            i2i_sim.setdefault(i, {})  # setdefault查找指定键,存在则返回对应的值,不存在则在字典中添加该键，值设为默认值
             for j in item_list:
                 if i == j:
                     continue
                 i2i_sim[i].setdefault(j, 0)
-                i2i_sim[i][j] += 1 / math.log(len(item_list) + 1)      # 打压活跃用户
+                i2i_sim[i][j] += 1 / math.log(len(item_list) + 1)  # 打压活跃用户
 
     i2i_sim_ = i2i_sim.copy()
     for i, related_items in i2i_sim.items():
         for j, wij in related_items.items():
-            i2i_sim_[i][j] = wij / math.sqrt(item_cnt[i] * item_cnt[j])     # 打压热门物品
+            i2i_sim_[i][j] = wij / math.sqrt(item_cnt[i] * item_cnt[j])  # 打压热门物品
 
     # 将得到的相似性矩阵保存到本地
     # pickle.dump(i2i_sim_, open('itemcf_i2i_sim.pkl', 'wb'))
     return i2i_sim_
 
 
-def ItemCF_rec(user_id, user_item_dict, i2i_sim, sim_item_topk, recall_item_num, item_topk_click):
+def ItemCF_rec(user_id, user_item_dict, i2i_sim, last_n, sim_item_topk, recall_item_num, item_topk_click):
     """
-        召回
-        输入： user_id: 用户id，
-        :param user_item_dict: {user1: {item1, item2..}...}
-        :param i2i_sim: 字典，文章相似性矩阵
-        :param sim_item_topk: 整数， 选择与当前文章最相似的前k篇文章
-        :param recall_item_num: 整数， 最后的召回文章数量
-        :param item_topk_click: 列表，点击次数最多的文章列表，用户召回补全
-        返回: 召回的文章列表 {item1: score1, item2: score2...}
+    根据历史交互的n个物品，每个物品找回最相似的k个物品，计算nk个物品的兴趣分数，取前num个作为召回结果
+    :param user_id: 用户id
+    :param user_item_dict: {user1: {item1, item2..}...}
+    :param i2i_sim: 字典，物品相似性矩阵
+    :param last_n: 最近交互的n个物品
+    :param sim_item_topk: 与当前物品最相似的前k个物品
+    :param recall_item_num: 最后的召回物品数量
+    :param item_topk_click: 热门物品池，用于补充
+    :return: 召回的物品列表 {item1: score1, item2: score2...}
     """
-
     # 获取用户历史交互的文章
-    user_hist_items = user_item_dict[user_id]
+    user_hist_items = list(user_item_dict[user_id])
+    # 不够n个就用全部，超过就取n个
+    if len(user_hist_items) > last_n:
+        n_hist_items = user_hist_items[:last_n]
+    else:
+        n_hist_items = user_hist_items
 
     item_rank = {}
-    for loc, i in enumerate(user_hist_items):
+    for loc, i in enumerate(n_hist_items):
         # 每个历史交互物品都找最相似的k个
         for j, wij in sorted(i2i_sim[i].items(), key=lambda x: x[1], reverse=True)[:sim_item_topk]:
             if j in user_hist_items:
                 continue
 
             item_rank.setdefault(j, 0)
-            item_rank[j] += wij     # 找出来的物品的总分
+            item_rank[j] += wij  # 找出来的物品的总分
 
-    # 不足10个，用热门商品补全
+    # 不足，用热门商品补充
     if len(item_rank) < recall_item_num:
         for i, item in enumerate(item_topk_click):
             if item in item_rank.items():  # 填充的item应该不在原来的列表中
@@ -79,25 +84,27 @@ def ItemCF_rec(user_id, user_item_dict, i2i_sim, sim_item_topk, recall_item_num,
 
 if __name__ == '__main__':
     # 交互记录
-    data = {1: {1, 3, 5, 7, 10},
-            2: {1, 2, 3, 4, 6, 7, 8},
-            3: {1, 4, 7, 10},
-            4: {2, 3, 4, 5, 9},
-            5: {4, 5, 6, 9, 10},
-            6: {3, 7},
-            7: {4, 6, 8, 10}}
-    hot_items = {3, 100, 1000}      # 热门物品池
+    user_item = {
+        1: {1, 3, 5, 7, 10},
+        2: {1, 2, 3, 4, 6, 7, 8},
+        3: {1, 4, 7, 10},
+        4: {2, 3, 4, 5, 9},
+        5: {4, 5, 6, 9, 10},
+        6: {3, 7},
+        7: {4, 6, 8, 10}
+    }
+    hot_items = {3, 100, 1000}  # 热门物品池
 
-    sim_matrix = ItemCF_sim(data)
+    sim_matrix = ItemCF_sim(user_item)
     # 线上召回通常在计算相似度矩阵后维护一个索引{item: {item1, item2 ...}，快速找到top_k相似物品
-    sim_k = 2           # 每篇文章找k篇相似的
-    recall_n = 3        # 一共召回n篇
+    last_n = 5  # 最近的n个交互
+    sim_k = 2  # 每个物品找k个相似的
+    recall_num = 3  # 一共召回num个
     userid = 1
-    recall_items = ItemCF_rec(userid, data, sim_matrix, sim_k, recall_n, hot_items)
+    recall_items = ItemCF_rec(userid, user_item, sim_matrix, last_n, sim_k, recall_num, hot_items)
     print(recall_items)
 
 # ItemCF只要两个物品重合比例高的用户就判定为相似，假如用户在同一个小圈子（同一个群，被分享）
 # 那么可能物品本来并不会有关联的物品因为它们得到了关联（不是因为用户兴趣而关联的）
 # 改进：Swing模型为了防止ItemCF重合的用户来自同一个圈子，将用户重合度加入相似度的计算公式
 # 统计同时喜欢i和喜欢j的用户集合，统计其中每两个用户的重合度，作为相似度的惩罚。
-
--- UserCF.py
+++ UserCF.py
@@ -1,38 +1,26 @@
-def get_user_activate_degree_dict(all_click_df):
-    all_click_df_ = all_click_df.groupby('user_id')['click_article_id'].count().reset_index()
-
-    # 用户活跃度归一化
-    mm = MinMaxScaler()
-    all_click_df_['click_article_id'] = mm.fit_transform(all_click_df_[['click_article_id']])
-    user_activate_degree_dict = dict(zip(all_click_df_['user_id'], all_click_df_['click_article_id']))
-
-    return user_activate_degree_dict
+import math
+import pickle
+from collections import defaultdict
+from tqdm import tqdm
 
 
-def usercf_sim(all_click_df, user_activate_degree_dict):
+def UserCF_sim(item_user_dict):
     """
-        用户相似性矩阵计算
-        :param all_click_df: 数据表
-        :param user_activate_degree_dict: 用户活跃度的字典
-        return 用户相似性矩阵
-
-        思路: 基于用户的协同过滤(详细请参考上一期推荐系统基础的组队学习) + 关联规则
+    用户相似性矩阵计算，与itemcf几乎一样
+    :param item_user_dict: {{item1: user1, user2...}, item2:...}
+    :return: 用户相似度矩阵
     """
-    item_user_time_dict = get_item_user_time_dict(all_click_df)
-
     u2u_sim = {}
     user_cnt = defaultdict(int)
-    for item, user_time_list in tqdm(item_user_time_dict.items()):
-        for u, click_time in user_time_list:
+    for item, user_list in tqdm(item_user_dict.items()):
+        for u in user_list:
             user_cnt[u] += 1
             u2u_sim.setdefault(u, {})
-            for v, click_time in user_time_list:
+            for v in user_list:
                 u2u_sim[u].setdefault(v, 0)
                 if u == v:
                     continue
-                # 用户平均活跃度作为活跃度的权重，这里的式子也可以改善
-                activate_weight = 100 * 0.5 * (user_activate_degree_dict[u] + user_activate_degree_dict[v])
-                u2u_sim[u][v] += activate_weight / math.log(len(user_time_list) + 1)
+                u2u_sim[u][v] += 1 / math.log(len(user_list) + 1)
 
     u2u_sim_ = u2u_sim.copy()
     for u, related_users in u2u_sim.items():
@@ -40,6 +28,78 @@ def usercf_sim(all_click_df, user_activate_degree_dict):
             u2u_sim_[u][v] = wij / math.sqrt(user_cnt[u] * user_cnt[v])
 
     # 将得到的相似性矩阵保存到本地
-    pickle.dump(u2u_sim_, open(save_path + 'usercf_u2u_sim.pkl', 'wb'))
-
+    # pickle.dump(u2u_sim_, open('usercf_u2u_sim.pkl', 'wb'))
     return u2u_sim_
+
+
+def UserCF_rec(user_id, user_item_dict, u2u_sim, last_n, sim_user_topk, recall_item_num, item_topk_click):
+    """
+    每个用户找到最相似的k个用户，找它们近期最感兴趣的n个物品，取分数最高的nk个物品作为召回结果
+    :param user_id: 用户id
+    :param user_item_dict: {user: {item1, item2,...}}
+    :param u2u_sim: 用户相似度矩阵
+    :param last_n: 近期n个交互
+    :param sim_user_topk: 最相似的k个用户
+    :param recall_item_num: 召回的物品数量
+    :param item_topk_click: 热门池
+    :return: 召回的物品列表 {item1: score1, item2: score2...}
+    """
+    item_rank = {}
+    # 当前用户交互过的物品
+    now_user_items = user_item_dict[user_id]
+    # 遍历前k个相似用户
+    for u, wij in sorted(u2u_sim[user_id].items(), key=lambda x: x[1], reverse=True)[:sim_user_topk]:
+        # 每个用户找近期交互的n个物品
+        u_hist_items = list(user_item_dict[u])
+        # 不够n个就用全部，超过就取n个
+        if len(u_hist_items) > last_n:
+            u_hist_items = u_hist_items[:last_n]
+        # 遍历每个物品，加分
+        for i in u_hist_items:
+            item_rank.setdefault(i, 0)
+            item_rank[i] += wij
+
+    # 不足用热门商品补全
+    if len(item_rank) < recall_item_num:
+        for i, item in enumerate(item_topk_click):
+            if item in item_rank.items():  # 填充的item应该不在原来的列表中
+                continue
+            item_rank[item] = - i - 100  # 给个负数，让热门物品排在最后
+            if len(item_rank) == recall_item_num:
+                break
+
+    item_rank = sorted(item_rank.items(), key=lambda x: x[1], reverse=True)[:recall_item_num]
+    return item_rank
+
+
+if __name__ == '__main__':
+    # 交互记录
+    user_item = {
+        1: {1, 3, 5, 7, 10},
+        2: {1, 2, 3, 4, 6, 7, 8},
+        3: {1, 4, 7, 10},
+        4: {2, 3, 4, 5, 9},
+        5: {4, 5, 6, 9, 10},
+        6: {3, 7},
+        7: {4, 6, 8, 10}
+    }
+    hot_items = {3, 100, 1000}  # 热门物品池
+    item_user = {
+        1: {1, 2, 3},
+        2: {2, 4},
+        3: {1, 2, 4, 6},
+        4: {2, 3, 4, 5},
+        5: {1, 4, 5},
+        6: {2, 5, 7},
+        7: {2, 3, 6},
+        8: {2, 7},
+        9: {4, 5},
+        10: {1, 3, 5, 7}
+    }
+    sim_matrix = UserCF_sim(item_user)
+    last_n = 5  # 最近的n个交互
+    sim_k = 2  # 每个物品找k个相似的
+    recall_num = 3  # 一共召回num个
+    userid = 1
+    recall_items = UserCF_rec(userid, user_item, sim_matrix, last_n, sim_k, recall_num, hot_items)
+    print(recall_items)
